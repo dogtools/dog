6ec0bbf9a89e0c36b404d35111f287b30da68f9b
Xavi S.B <xavi@typeform.com>
2016-06-10 12:33:13 +0200

    First commit

---
+++ exec.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func execTask(task string, script []byte) (duration time.Duration) {
+	// TODO check that parameters not empty
+
+	// Check that executor exists
+	binary, err := exec.LookPath("sh")
+	if err != nil {
+		panic(err)
+	}
+
+	// Write the script to disk
+	path := "/tmp/dog-" + task + ".sh"
+	err = ioutil.WriteFile(path, script, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// Define the command to launch and its arguments
+	cmd := exec.Command(binary, path)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	// Collect and print STDOUT
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("%s\n", scanner.Text())
+		}
+	}()
+
+	// Start and wait until it finishes
+	startTime := time.Now()
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		panic(err)
+	}
+	duration = time.Now().Sub(startTime)
+
+	// Remove temporary script
+	err = os.Remove(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return duration
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type task struct {
+	task        string
+	description string
+	run         []byte
+}
+
+var taskList = map[string]task{
+	"hello": task{
+		description: "Say Hello!",
+		run:         []byte("echo hello world"),
+	},
+	"bye": task{
+		description: "Good Bye!",
+		run:         []byte("echo bye cruel world"),
+	},
+	"find": task{
+		description: "List all files in $HOME directory",
+		run:         []byte("find /home/xavi"),
+	},
+}
+
+func main() {
+	arg := os.Args[1]
+	if arg == "list" || arg == "help" {
+		for k, t := range taskList {
+			fmt.Printf("%s\t%s\n", k, t.description)
+		}
+	} else {
+		// TODO check that task exists
+		task := taskList[arg].task
+		run := taskList[arg].run
+		duration := execTask(task, run)
+		fmt.Println(duration.Seconds())
+	}
+}
